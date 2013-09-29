096b1441d0b6b3481ac8b4d787b8f168a7ae85f8
Jeffrey Chupp <jeff@semanticart.com>
2013-09-29 14:46:37 -0400

    adds formats

--- README.md
+++ README.md
@@ -10,7 +10,17 @@ but you should probably just use the included titles.csv file and leave their se
 titles.csv
 ============
 
-the results of scrape.rb (sorted by most popular)
+The results of scrape.rb (sorted by most popular). The final column is the format code. Here's the known codes so far:
+
+* format50: ebooks (adobe pdf only)
+* format302: Disney Online Book
+* format410: ebooks (not overdrive READ or kindle)
+* format420: ebooks (kindle, overdrive READ, adobe epub &/or adobe pdf)
+* format425: audiobook
+* format450: ebooks (open pdf)
+* format610: ebooks (overdrive READ + various formats, but not kindle)
+* format810: ebooks (open epub only)
+* format@@FORMAT_TYPE_ID@@: ebooks (various formats)
 
 goodreads-sift.rb
 ============
@@ -27,8 +37,6 @@ You can pass `--open` as the last argument to have the script open all the resul
 
 Note: We're using case-insensitive substring matching for titles and authors. This obviously isn't bullet-proof but should find a lot of matches where goodreads includes a subtitle (or series) and the library does not. Improvements to matching are welcome.
 
+If you only want certain formats, you can just pipe the output into grep. e.g. for audiobooks
 
-TODO
-============
-
-* handle audiobooks separately in both the scrape and the sift.
+`ruby goodreads-sift.rb goodreads_export.csv to-read | grep format425`
--- scrape.rb
+++ scrape.rb
@@ -2,36 +2,49 @@ require 'open-uri'
 require 'csv'
 require 'nokogiri'
 
+delay = [ENV['DELAY'].to_i || 1].min
+
 def parse_page(content)
-  Nokogiri::HTML(content).css('.search-results-grid-info').map do |result|
+  Nokogiri::HTML(content).css('.search-result-cover').map do |result|
     title = result.css('.trunc-title-line-grid').text
     author = result.css('.trunc-author-line-grid').text
     url = "http://digital.minlib.net/5BCFCE90-489E-4FC4-BB83-F9EE12D1250C/10/50/en/" + result.css('.trunc-title-line-grid a').first['href']
+    fmtid = result.css('.pageturn3-container img').first["data-fmtid"]
 
-    [title, author, url]
+    [title, author, url, fmtid]
   end
 end
 
-catalog_url = "http://digital.minlib.net/5BCFCE90-489E-4FC4-BB83-F9EE12D1250C/10/50/en/SearchResultsGrid.htm?SearchID=14055122s&SortBy=relevancy&Page=%s"
+catalog_url = "http://digital.minlib.net/5BCFCE90-489E-4FC4-BB83-F9EE12D1250C/10/50/en/SearchResultsGrid.htm?SearchID=14069976s&SortBy=rank&SortOrder=desc&Page=%s"
 
 page = 1
 max_page = nil
 
 CSV.open("titles.csv", "w") do |csv|
-  while max_page.nil? || page < max_page
+  while max_page.nil? || page <= max_page
     puts "page ##{page}"
 
-    content = open(catalog_url % page).read
+    begin
+      content = open(catalog_url % page).read
 
-    if max_page.nil?
-      max_page ||= Nokogiri::HTML(content).css('#pageLinks a').last["href"].match(/Page=(\d+)/)[1].to_i
-      puts "max page is #{max_page}"
+      if max_page.nil?
+        max_page ||= Nokogiri::HTML(content).css('#pageLinks a').last["href"].match(/Page=(\d+)/)[1].to_i
+        puts "max page is #{max_page}"
+      end
+
+      results = parse_page(content)
+    rescue => ex
+      p ex
+      sleep delay
+      puts "retrying"
+      retry
     end
 
-    parse_page(content).each do |result|
+    results.each do |result|
       csv << result
     end
 
     page += 1
+    sleep delay
   end
 end
