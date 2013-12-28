aa4814b35d719a6c2e2d51efc9916be5052a86c3
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2013-12-28 11:41:10 -0500

    Updates for new Overdrive url and page format, along with fresh scraping results.

--- scrape.rb
+++ scrape.rb
@@ -6,45 +6,58 @@ delay = [ENV['DELAY'].to_i || 1].min
 
 def parse_page(content)
   Nokogiri::HTML(content).css('.search-result-cover').map do |result|
-    title = result.css('.trunc-title-line-grid').text
-    author = result.css('.trunc-author-line-grid').text
-    url = "http://digital.minlib.net/5BCFCE90-489E-4FC4-BB83-F9EE12D1250C/10/50/en/" + result.css('.trunc-title-line-grid a').first['href']
+    title = result.css('.trunc-title-line').text
+    author = result.css('.trunc-author-line').text
+    url = "http://digital.minlib.net/C29B15FA-BAF1-4A87-9151-46B10B7AF0B5/10/50/en/" + result.css('.trunc-title-line a').first['href']
     fmtid = result.css('.pageturn3-container img').first["data-fmtid"]
 
     [title, author, url, fmtid]
   end
 end
 
-catalog_url = "http://digital.minlib.net/5BCFCE90-489E-4FC4-BB83-F9EE12D1250C/10/50/en/SearchResultsGrid.htm?SearchID=14069976s&SortBy=rank&SortOrder=desc&Page=%s"
 
-page = 1
-max_page = nil
 
 CSV.open("titles.csv", "w") do |csv|
-  while max_page.nil? || page <= max_page
-    puts "page ##{page}"
+  types = {
+    fiction:          '16334872s',
+    non_fiction:      '16335001s',
+    kids_fiction:     '16335003s',
+    audio_fiction:    '16335005s',
+    audio_nonfiction: '16335006s'
+  }
 
-    begin
-      content = open(catalog_url % page).read
+  types.each do |name, id|
+    puts "processing #{name}"
 
-      if max_page.nil?
-        max_page ||= Nokogiri::HTML(content).css('#pageLinks a').last["href"].match(/Page=(\d+)/)[1].to_i
-        puts "max page is #{max_page}"
+    catalog_url = "http://digital.minlib.net/C29B15FA-BAF1-4A87-9151-46B10B7AF0B5/10/50/en/SearchResults.htm?SearchID=#{id}&SortBy=globalrank&SortOrder=desc&Page=%s"
+    page = 1
+    max_page = nil
+
+    while max_page.nil? || page <= max_page
+      puts "page ##{page}"
+
+      begin
+        content = open(catalog_url % page).read
+
+        if max_page.nil?
+          max_page ||= Nokogiri::HTML(content).css('#pageLinks a').last["href"].match(/Page=(\d+)/)[1].to_i
+          puts "max page is #{max_page}"
+        end
+
+        results = parse_page(content)
+      rescue => ex
+        p ex
+        sleep delay
+        puts "retrying"
+        retry
       end
 
-      results = parse_page(content)
-    rescue => ex
-      p ex
+      results.each do |result|
+        csv << result
+      end
+
+      page += 1
       sleep delay
-      puts "retrying"
-      retry
     end
-
-    results.each do |result|
-      csv << result
-    end
-
-    page += 1
-    sleep delay
   end
 end
